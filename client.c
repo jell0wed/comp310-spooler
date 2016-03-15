51478d7312af4e52a3894d4438bbfb699953c16f
Jeremie P <jeremie@weboxigen.com>
2016-03-14 23:14:55 -0400

    fix

--- client.c
+++ client.c
@@ -83,7 +83,7 @@ print_job create_print_job(print_client* client, int pagec) {
  * Enqueue the given job in the spooler buffer.
  */
 void put_job(print_client* client, shared_spooler_data* spooler, print_job* job) {
-    printf("Waiting for server; queue is full.");
+    printf("Waiting for server; queue is full. \n");
     enqueue_job(spooler, job);
 
     printf("Client %d has %d pages to print. Puts request in Buffer[%d]\n",
